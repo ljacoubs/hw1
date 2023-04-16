9740a8f59aa1312f5774721c8845f066164473d3
Lou Jacoubs <lou.jacoubs@kellogg.northwestern.edu>
2023-04-16 22:11:27 +0000

    added actors for the dark knight

--- kmdb.sql
+++ kmdb.sql
@@ -71,53 +71,114 @@ VALUES (
 INSERT INTO actors (
 first_name,
 last_name,
-character_id,
-movie_id
+character_id
 )
 VALUES (
 "Christian",
 "Bale",
-"1",
 "1"
 );
 
 INSERT INTO actors (
 first_name,
 last_name,
-character_id,
-movie_id
+character_id
 )
 VALUES (
 "Michael",
 "Caine",
-"2",
-"1"
+"2"
 );
 INSERT INTO actors (
 first_name,
 last_name,
-character_id,
-movie_id
+character_id
 )
 VALUES (
 "Liam",
 "Neeson",
-"3",
-"1"
+"3"
 );
 INSERT INTO actors (
 first_name,
 last_name,
-character_id,
-movie_id
+character_id
 )
 VALUES (
 "Katie",
 "Holmes",
-"4",
-"1"
+"4"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Gary",
+"Oldman",
+"5"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Heath",
+"Ledger",
+"6"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Aaron",
+"Eckhart",
+"7"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Maggie",
+"Gyllenhaal",
+"8"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Tom",
+"Hardy",
+"9"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Joseph",
+"Gordon-Levitt",
+"10"
+);
+INSERT INTO actors (
+first_name,
+last_name,
+character_id
+)
+VALUES (
+"Anne",
+"Hathaway",
+"11"
 );
 
-select first_name, last_name, character_id, movie_id
-from actors
-;
+
+
